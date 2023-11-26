c873184e1fc4038c2cfd0baa2a0271b0927a4b64
jun0id <150011569+jun0id@users.noreply.github.com>
2023-11-26 15:50:07 +0400

    Two Sum

---
+++ Two Sum.py
@@ -0,0 +1,28 @@
+#Two Sum
+
+class Solution:
+    def twoSum(self, nums, target):
+        num_dict = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
+        
+        return None
+
+
+solution = Solution()
+
+nums1 = [2, 7, 11, 15]
+target1 = 9
+print(solution.twoSum(nums1, target1))
+
+nums2 = [3, 2, 4]
+target2 = 6
+print(solution.twoSum(nums2, target2))
+
+nums3 = [3, 3]
+target3 = 6
+print(solution.twoSum(nums3, target3))
